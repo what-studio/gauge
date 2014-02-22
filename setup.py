e1a38a3085b5381b5f61a3aeb5d0fdf6fff80502
Heungsub Lee <sub@subl.ee>
2014-02-23 02:54:25 +0900

    add setup.py 	visual.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+from __future__ import with_statement
+import re
+from setuptools import setup
+from setuptools.command.test import test
+
+
+# detect the current version
+with open('gauge.py') as f:
+    version = re.search(r'__version__\s*=\s*\'(.+?)\'', f.read()).group(1)
+assert version
+
+
+# use pytest instead
+def run_tests(self):
+    raise SystemExit(__import__('pytest').main([]))
+test.run_tests = run_tests
+
+
+setup(
+    name='gauge',
+    version=version,
+    license='BSD',
+    author='Heungsub Lee',
+    author_email=re.sub('((sub).)(.*)', r'\2@\1.\3', 'sublee'),
+    url='https://github.com/sublee/gauge',
+    description='Deterministic linear gauge library',
+    long_description=__doc__,
+    platforms='any',
+    py_modules=['gauge'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Topic :: Games/Entertainment'],
+    install_requires=['blist'],
+    tests_require=['pytest'],
+)
