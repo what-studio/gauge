d2531d6872de1ea4dc503414cd57ac47906de42b
Heungsub Lee <sub@subl.ee>
2014-10-16 03:22:12 +0900

    add hypergauge test cases to gaugetest.py

--- gaugetest.py
+++ gaugetest.py
@@ -379,3 +379,50 @@ def test_extensibility_of_make_momentum():
     g = MyGauge(0, 10, at=0)
     m = g.add_momentum(3, 2, 1)
     assert m == (1, 2, 3)
+
+
+def test_unidirectional_hypergauge():
+    g = Gauge(12, 100, at=0)
+    g.add_momentum(+1, since=1, until=6)
+    g.add_momentum(-1, since=3, until=8)
+    # case 1
+    g.max = Gauge(15, 15, at=0)
+    g.max.add_momentum(-1, until=5)
+    assert g.determine() == [
+        (0, 12), (1, 12), (2, 13), (3, 12), (5, 10), (6, 10), (8, 8)]
+    assert g.max.determine() == [(0, 15), (5, 10)]
+    # case 2
+    g.max = Gauge(15, 15, at=0)
+    g.max.add_momentum(-1, until=4)
+    g.max.add_momentum(+1, since=4, until=6)
+    assert g.determine() == [
+        (0, 12), (1, 12), (2, 13), (3, 12), (4, 11), (6, 11), (8, 9)]
+    # case 3
+    g.set_max(10, at=0)
+    assert g.determine() == [
+        (0, 12), (1, 12), (3, 12), (5, 10), (6, 10), (8, 8)]
+    g.set_max(Gauge(10, 100, at=0), at=0)
+    assert g.determine() == [
+        (0, 12), (1, 12), (3, 12), (5, 10), (6, 10), (8, 8)]
+    # case 4
+    g.max = Gauge(15, 15, at=0)
+    g.max.add_momentum(-1)
+    assert g.determine() == [
+        (0, 12), (1, 12), (2, 13), (3, 12), (6, 9), (8, 7), (15, 0)]
+
+
+def test_bidirectional_hypergauge():
+    ceil = Gauge(10, 10, at=0)
+    ceil.add_momentum(-1, since=0, until=4)
+    ceil.add_momentum(+1, since=6, until=7)
+    floor = Gauge(0, 10, at=0)
+    floor.add_momentum(+1, since=1, until=6)
+    floor.add_momentum(-1, since=6, until=8)
+    g = Gauge(5, ceil, floor, at=0)
+    g.add_momentum(+1, since=0, until=3)
+    g.add_momentum(-1, since=3, until=6)
+    g.add_momentum(+1, since=6, until=9)
+    g.add_momentum(-1, since=9, until=12)
+    assert g.determine() == [
+        (0, 5), (2.5, 7.5), (3, 7), (4, 6), (5.5, 4.5),
+        (6, 5), (8, 7), (9, 7), (12, 4)]
