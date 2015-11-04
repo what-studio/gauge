a56721de0e8e00c6f60f826744744eeef7b025ca
Heungsub Lee <sub@subl.ee>
2015-11-04 22:10:40 +0900

    Test on Python 3.5 at Travis CI

---
+++ gaugebenchmark.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pickle
+from random import Random
+
+import pytest
+
+from gauge import Gauge
+
+
+@pytest.mark.parametrize('length', [0, 1, 100, 10000])
+def test_pickle(benchmark, length):
+    g = Gauge(0, 10, at=0)
+    r = Random(length)
+    for x in range(length):
+        since = r.randrange(1000)
+        until = since + 1 + r.randrange(1000)
+        g.add_momentum(r.uniform(-10, +10), since=since, until=until)
+    benchmark(lambda: pickle.loads(pickle.dumps(g)))
