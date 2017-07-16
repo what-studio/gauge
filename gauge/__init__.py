736d4b8207cb63bda86f4338c08cc8719416fbe7
Heungsub Lee <sub@subl.ee>
2017-07-17 03:37:08 +0900

    Fix pickle failure in PyPy

--- gauge/__init__.py
+++ gauge/__init__.py
@@ -17,3 +17,16 @@ from gauge.core import Gauge, Momentum
 
 
 __all__ = ['Gauge', 'Momentum', 'ERROR', 'OK', 'ONCE', 'CLAMP']
+
+
+try:
+    import __pypy__
+except ImportError:
+    pass
+else:
+    # Here's a workaround which makes :class:`Gauge` be picklable in PyPy.
+    # ``Gauge.__module__`` and ``__name__`` should be ``'gauge.core'`` and
+    # ``'Gauge'``.  But in PyPy, unlike CPython, they are ``'gauge'`` and
+    # ``'core.Gauge'``.
+    locals()['core.Gauge'] = Gauge
+    del __pypy__
